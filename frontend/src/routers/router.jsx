c2813b5ab1563a08f761c3ea018f7e913208d8f3
varun77x <varun77x@gmail.com>
2024-10-18 19:02:34 +0530

    Login/register page and setting up firebase auth

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
 
 const router = createBrowserRouter([
     {
     path:"/",
-    element:<span>hi</span>
-    },
+    element:<App/>,
+    children:[
+        {
+            path:"/",
+            element:<p>home</p>
+        },
+        {
+            path:"/login",
+            element:<p>login</p>
+        },
+        {
+            path:"/register",
+            element:<p>register</p>
+        }
+    ]
+    }
 ]);
 
 export default router;
