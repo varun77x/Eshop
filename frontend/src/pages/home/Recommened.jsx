8ee2ea06b035c0e4f16a2ed9ec50c3028b155286
varun77x <varun77x@gmail.com>
2024-12-10 23:59:05 +0530

    some twitches

--- frontend/src/pages/home/Recommened.jsx
+++ frontend/src/pages/home/Recommened.jsx
@@ -10,19 +10,14 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import BookCard from '../books/BookCard';
+import { useFetchAllBooksQuery } from '../../redux/features/books/booksApi';
 
 
 
 const Recommened = () => {
 
 
-    const [books, setBooks] = useState([]);
-
-    useEffect(() => {
-        fetch(books.json)
-            .then(res => res.json())
-            .then((data) => setBooks(data));
-    }, []);
+    const {data: books = []} = useFetchAllBooksQuery();
     return (
         <div className='py-16'>
             <h2 className='text-3xl font-semibold mb-6'>Recommended for you </h2>
